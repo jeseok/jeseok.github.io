667f34036e6a0ee638ed34f704924d26f7f42be5
jeseok.koo <jeseok.koo@navercorp.com>
2020-08-21 00:11:02 +0900

    update

--- src/app/data/data.js
+++ src/app/data/data.js
@@ -36,7 +36,7 @@ export default {
         en: "A software developer living in South Korea. \nI’m passionate to build testable, reusable, manageable, and scalable software using classical programming practices with modern languages and tools such as automated task management and continuous integration."
       },
       socialLink: {
-        url : "https://github.com/jeseok",
+        url : "https://kr.linkedin.com/in/jeseok-koo-bb16b767",
         username: "@jeseok"
       },
       contact: {
@@ -48,14 +48,14 @@ export default {
       'Beats by Dre','Campbell', 'Comcast', 'Sunoco','Philadelphia Museum of Arts','Technical.ly(Philly)','Philadelphia International Festival of the Arts', 'Subcircle', '...'
     ],
     specialties: [
-      'iOS', 'Swift', 'Objective-C', 'Javascript', 'React', 'Python', 'OpenCV', 'OpenFrameworks', 'Arduino', '...'
+      'iOS', 'Swift', 'Objective-C', 'Java', 'Spring Boot', 'MySql', 'Javascript', 'React', '...'
     ],
     experience : [
       {
         time: "Aug 2017 - Now",
         title : "Naver",
         link : "https://www.navercorp.com/en/index.nhn",
-        etc: "iOS Developer"
+        etc: "Software Developer"
       },
       {
         time: "Jan 2014 - April 2017",
@@ -78,6 +78,21 @@ export default {
     ],
     projects : [
       {
+        time: "2019 ~ Now",
+        title: "Influencer Search at Naver",
+        link : "https://in.naver.com/"
+      },
+      {
+        time: "2018 - 2019",
+        title: "Moment at Naver",
+        link : "https://campaign.naver.com/blogmoment/main/"
+      },
+      {
+        time: "2017 - 2018",
+        title: "Smart-Editor ONE at Naver",
+        link : "https://smarteditor.naver.com/desktop/"
+      },
+      {
         time: "April 2017",
         title: "Core Campaign site for Philadelphia Museum of Arts",
         link : "http://www.philamuseum.org/ourfuture/"
@@ -85,27 +100,27 @@ export default {
       {
         time: "Mar 2017",
         title: "iPad Kiosk for Comcast NBCUniversal",
-        link : "http://www.a-g.com/thinking/2017/03/allen-and-gerritsen-crafts-success-stories-with-comcast-nbcuniversal-for-sxsw-2017/"
+        link : "https://www.a-g.com/news/a-g-crafts-success-stories-with-comcast-nbcuniversal"
       },
       {
         time: "Nov 2016",
-        title : "Installations for Philadelphia Museum of Arts",
-        link : "http://www.a-g.com/work/philadelphia-museum-of-art",
+        title : "Installations for Philadelphia Museum of Arts" ,
+        link : "https://www.courierpostonline.com/story/life/2016/11/03/mexican-modernism-philadelphia-museum-of-art/93241136/",
       },
       {
         time: "Feb 2015",
         title : "Snackbot for Campbell",
-        link : "http://www.a-g.com/work/campbell/",
+        link : "https://www.youtube.com/watch?v=5eot4GGYPnE&feature=emb_title",
       },
       {
         time: "July 2014",
         title : "Pic Nix for A&G Labs",
-        link : "http://a-glabs.com/picnix/",
+        link : "https://www.youtube.com/watch?v=qgV_z6ZpBks",
       },
       {
         time: "Dec 2013",
         title : "Led Tree for Beats By Dre",
-        link : "http://www.a-g.com/thinking/2013/12/aandg-beatsbydre-team-up-social-powered-holiday-experience/",
+        link : "https://www.youtube.com/watch?v=892k5MDn_R0&feature=emb_title",
       },
       {
         time: "Mar 2013",
